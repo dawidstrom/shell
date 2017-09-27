de2eca56e7a154b1bfd08781ca8dbc18d2c1e950
David Ström <dawid.strom@gmail.com>
2017-09-27 10:19:14 +0200

    Updated documentation for working version of shell.

--- lsh.c
+++ lsh.c
@@ -51,17 +51,25 @@ int pi = 0;
 /* When non-zero, this global means the user is done using this program. */
 int done = 0;
 
+/*
+ * Description:
+ * Kills all running foreground processes.
+ */
 void
 sigint_handler(int sig)
 {
 	pi--;
-	for( pi; pi>=0; pi-- )
+	for ( pi; pi >= 0; pi-- )
 	{
 		kill(SIGTERM, pids[pi]);
 	}
 	pi++;
 }
 
+/*
+ * Description:
+ * Reaps background process so it doesn't become a zombie.
+ */
 void
 sigchld_handler(int sig)
 {
@@ -70,15 +78,16 @@ sigchld_handler(int sig)
 
 /*
  * Name: main
- *
- * Description: Gets the ball rolling...
- *
+ * 
+ * Description:
+ * Gets the ball rolling...
  */
 int main(void)
 {
   Command cmd;
   int n;
 
+  // Setup handlers for CTRL+C and background processes.
   signal(SIGINT, sigint_handler);
   signal(SIGCHLD, sigchld_handler);
 
@@ -128,19 +137,26 @@ int main(void)
 int
 execute(Command* cmd)
 {
-	// set fdin and fdout to STDIN and STDOUT if no redirect is given.
 	int fdin, fdout;
 
+	// set fdin and fdout to STDIN and STDOUT if no redirect is given.
 	fdin = cmd->rstdin == NULL ? STDIN_FILENO : open(cmd->rstdin, O_RDONLY);
 	fdout = cmd->rstdout == NULL ? STDOUT_FILENO : open(cmd->rstdout, O_WRONLY|O_CREAT);
 
+	// Keep track of the number of processes running in foreground.
 	int zero = 0;
 	int* size = &zero; 
+
+	// Recursivly execute all programs.
 	exec_rec(cmd->pgm, fdin, fdout, size, cmd->bakground);
-	for( int i = *size; i>0; i-- )
+
+	// Block untill all foreground processes have returned.
+	for ( int i = *size; i>0; i-- )
 	{
 		wait(NULL);
 	}
+
+	// Set number of processes running in foreground to 0. (All have exited)
 	pi = 0;
 	return 1;
 }
@@ -152,22 +168,27 @@ execute(Command* cmd)
  * pgm   - The program to execute.
  * fdin  - The file descriptor program should read input from.
  * fdout - The file descriptor program should write output to.
+ * size  - Counter for number of foreground processes spawned.
+ * bg    - Flag used to indicate processes should be spawned in background.
  *
  * Description: 
- * Runs a single program, reads input from fdin and writes 
- * output to fdout.
+ * Recursivly spawns all the program pointed to by pgm, reads input from 
+ * fdin and writes output to fdout.
  */
 void
 exec_rec(Pgm* pgm, int fdin, int fdout, int* size, int bg)
 {
+	// Only count foreground processes.
 	*size = bg ? 0 : (*size)+1;
-	if (strcmp(pgm->pgmlist[0], "exit") == 0)
+
+	// Handle built-in commands.
+	if ( strcmp(pgm->pgmlist[0], "exit") == 0 )
 	{
 		exit(0);
 	}
-	else if (strcmp(pgm->pgmlist[0], "cd") == 0)
+	else if ( strcmp(pgm->pgmlist[0], "cd") == 0 )
 	{
-		if (chdir(pgm->pgmlist[1]) == -1)
+		if ( chdir(pgm->pgmlist[1]) == -1 )
 			perror(pgm->pgmlist[1]);
 		return;
 	}
@@ -175,7 +196,8 @@ exec_rec(Pgm* pgm, int fdin, int fdout, int* size, int bg)
 	int fd[2];
 	pipe(fd);
 		
-	if( pgm->next != NULL )
+	// Recursivly spawn programs if not the last program.
+	if ( pgm->next != NULL )
 	{
 		exec_rec(pgm->next, fdin, fd[1], size, bg);
 		close(fd[1]);
@@ -183,25 +205,31 @@ exec_rec(Pgm* pgm, int fdin, int fdout, int* size, int bg)
 
 	int pid = fork();
 	
-	if( pid == 0 )
-	{	
-		if( bg )
+	// Spawn process which executes the given command.
+	if ( pid == 0 )
+	{
+		// Do not propagate signals to baCkground processes.
+		if ( bg )
 			setpgid(0,0);
 
-		if (pgm->next != NULL)
+		// Connect input/output properly of spawned processes.
+		if ( pgm->next != NULL )
 			dup2(fd[0], STDIN_FILENO);
 		else
 			dup2(fdin, STDIN_FILENO);
 		dup2(fdout, STDOUT_FILENO);
 		close(fd[0]);
-		if (execvp(pgm->pgmlist[0], pgm->pgmlist))
+
+		// Execute program and handle errors.
+		if ( execvp(pgm->pgmlist[0], pgm->pgmlist) )
 		{
 			perror(pgm->pgmlist[0]);
 			exit(0);	
 		}
 	}
 
-	if (!bg)
+	// Only store pid of foreground processes.
+	if ( !bg )
 	{
 		pids[pi] = pid;
 		pi++;
